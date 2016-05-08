aeda5ad09af51832087266b85bd985d19315bf0d
aviatordave <aviatordave@gmail.com>
2016-05-07 22:36:47 -0700

    Added Invert on Bluetooth Disconnect

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@ static Window *s_main_window;
 static TextLayer *s_time_layer, *s_UTC_layer, *s_date_layer;
 
 
+
+
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -39,48 +41,28 @@ strftime(date_buffer, sizeof(date_buffer), "%a-%d-%b", tick_time);
   text_layer_set_text(s_date_layer, date_buffer);
 }
 
-static void update_face() {
-  
-}
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-}
-
-static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
+static void update_face(GColor TextColor, GColor BackgroundColor) {
   //Set Colors  
-  GColor TextColor;
-  GColor BackgroundColor;
-    TextColor = GColorWhite;
-    BackgroundColor= GColorBlack;
+  //GColor TextColor;
+  //GColor BackgroundColor;
+    //TextColor = GColorWhite;
+   // BackgroundColor= GColorBlack;
   
   //Set Background Color
   window_set_background_color(s_main_window, BackgroundColor);
   
-  // Create the TextLayer with specific bounds for local Time
-  s_time_layer = text_layer_create(GRect(0, 45, bounds.size.w, 50));
-  
-    // Create the TextLayer with specific bounds for UTC Time
-  s_UTC_layer = text_layer_create(GRect(0, 143, bounds.size.w, 50));
-  
-  //Create the Date Layer
-  s_date_layer = text_layer_create(GRect(0, 90, bounds.size.w, 30));
+ 
 
   // Improve the layout to be more like a watchface for local time
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, TextColor);
-  text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Improve the layout to be more like a watchface for UTC time
   text_layer_set_background_color(s_UTC_layer, GColorClear);
   text_layer_set_text_color(s_UTC_layer, TextColor);
-  text_layer_set_text(s_UTC_layer, "00:00");
-  text_layer_set_font(s_UTC_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_font(s_UTC_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(s_UTC_layer, GTextAlignmentCenter);
   
   // Create date TextLayer
@@ -91,11 +73,48 @@ text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
 
   
+  
+}
+
+static void bluetooth_callback(bool connected) {
+  // Invert if disconnected
+  update_face(GColorWhite, GColorBlack);
+  
+  if(!connected) {
+    vibes_double_pulse();
+    update_face(GColorBlack, GColorWhite);
+  }
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+   // Create the TextLayer with specific bounds for local Time
+  s_time_layer = text_layer_create(GRect(0, 40, bounds.size.w, 50));
+  
+    // Create the TextLayer with specific bounds for UTC Time
+  s_UTC_layer = text_layer_create(GRect(0, 143, bounds.size.w, 50));
+  
+  //Create the Date Layer
+  s_date_layer = text_layer_create(GRect(0, 85, bounds.size.w, 30));
+  
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_UTC_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  
+  update_face( GColorWhite, GColorBlack);
+  
+  //Update Bluetooth
+  bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
+
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
@@ -123,6 +142,11 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  //Register Bluetooth Service
+  connection_service_subscribe((ConnectionHandlers) {
+    .pebble_app_connection_handler = bluetooth_callback
+  });
 }
 
 static void deinit() {
